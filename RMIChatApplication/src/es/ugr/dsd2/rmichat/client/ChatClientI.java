4f4cea22d2ce0b15389d4e20bff781cb91eeeb04
José Miguel Márquez Herreros <jmiguelmh3000@gmail.com>
2022-05-09 22:45:15 +0200

    Práctica 2
    Ejemplos implementados

--- RMIChatApplication/src/es/ugr/dsd2/rmichat/client/ChatClientI.java
+++
@@ -1,8 +0,0 @@
-package es.ugr.dsd2.rmichat.client;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ChatClientI extends Remote {
-	void retrieveMessage(String message) throws RemoteException;
-}
